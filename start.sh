322960c74c63db063d21a8569abaa61b5b4b185b
venu002 <137064595+venu002@users.noreply.github.com>
2024-11-07 14:50:23 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -d --name sonarqube-db -e POSTGRES_USER=sonar -e POSTGRES_PASSWORD=sonar -e POSTGRES_DB=sonarqube postgres:alpine
-
-sleep 2
-
-docker run -d --name sonarqube -p 9000:9000 --link sonarqube-db:db -e SONAR_JDBC_URL=jdbc:postgresql://db:5432/sonarqube -e SONAR_JDBC_USERNAME=sonar -e SONAR_JDBC_PASSWORD=sonar sonarqube
-
-docker ps -a
